65cbbab4a5c9b99b9c0f3736c247bef11761b56f
khoidm2004 <khoidm2004@gmail.com>
2024-12-13 06:27:41 +0200

    Undo replacing env variable

--- Dashboard/vite.config.js
+++ Dashboard/vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { loadEnv } from "vite";
 
-// Load environment variables from .env file
-export default defineConfig(({ mode }) => {
-  // Load .env file based on the mode (e.g., .env.development, .env.production)
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_API_URL,
+          target: "http://68.219.251.214",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
